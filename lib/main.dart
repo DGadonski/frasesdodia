d21ff50f690658712bc0d499d45321ee08bb4ec9
DGadonski <derek.gadonski@gmail.com>
2023-07-26 16:32:45 -0300

    Implementação da lógica.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-// ignore_for_file: prefer_const_constructors
-
+// ignore_for_file: prefer_const_constructors, prefer_final_fields
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 main(){
@@ -14,6 +14,34 @@ class Frasesdodia extends StatefulWidget {
 }
 
 class _FrasesdodiaState extends State<Frasesdodia> {
+
+  List<String> _frases = [
+    'Quem não se ira facilmente é melhor do que o homem valente, E quem controla a si mesmo é melhor do que quem conquista uma cidade. - Pro. 16:32',
+    'Mas, quanto a isso, quem semear parcimoniosamente colherá também parcimoniosamente, e quem semear generosamente colherá também generosamente. - 2 Cor 9:6',
+    'Portanto, todas as coisas que querem que os homens façam a vocês, façam também a eles. - Mat. 7:12',
+    'Quem mostra favor ao pobre empresta a Jeová, E Ele o recompensará por isso. - Pro. 19:17',
+  ];
+
+  String _fraseGerada = 'Clique abaixo para gerar uma frase.';
+
+  gerarFrase () {
+
+    int numeroAleatorio = Random().nextInt(_frases.length);
+
+    setState(() {
+      _fraseGerada = _frases[numeroAleatorio];
+    });
+
+  }
+
+  resetFrase () {    
+
+    setState(() {
+      _fraseGerada = 'Clique abaixo para gerar uma frase.';
+    });
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -25,21 +53,15 @@ class _FrasesdodiaState extends State<Frasesdodia> {
         ),
         body: Container(
           width: double.infinity, // Ou poderia colocar o Container dentro de um Center.
-          padding: EdgeInsets.all(16),
-          decoration: BoxDecoration(
-            border: Border.all(
-              color: Colors.red,
-              width: 3
-            )
-          ),
+          padding: EdgeInsets.all(16),          
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Image.asset("images/logo.png"),
               Text(
-                'Clique abaixo para gerar uma frase.',
-                textAlign: TextAlign.justify,
+                _fraseGerada,
+                textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 17,
                   fontStyle: FontStyle.italic
@@ -49,9 +71,23 @@ class _FrasesdodiaState extends State<Frasesdodia> {
                 style: ButtonStyle(
                   backgroundColor: MaterialStatePropertyAll(Colors.green) 
                   ),                                
-                onPressed: (){}, 
+                onPressed: gerarFrase, 
                 child: 
-                Text('Nova frase',
+                Text('Frase nova',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold 
+                ),
+                )
+                ),
+              ElevatedButton(                
+                style: ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll(Colors.blue) 
+                  ),                                
+                onPressed: resetFrase, 
+                child: 
+                Text('Reset',
                 style: TextStyle(
                   color: Colors.white,
                   fontSize: 15,
